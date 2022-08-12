d709834b381201d9cc05191436074f02564ccdbc
hzhilong <1107495265@qq.com>
2022-08-12 15:38:19 +0800

    更新弹窗和通知的链接颜色。新增bat快速复制主题文件。

---
+++ 快速复制主题文件.bat
@@ -0,0 +1,17 @@
+@echo off
+color 3F
+mode con cols=90 lines=30
+:: 申请管理员权限
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+:: ======================================================================
+
+copy /y .\themes\ybgnb-night.css "%appdata%\Typora\themes\"
+
+pause
